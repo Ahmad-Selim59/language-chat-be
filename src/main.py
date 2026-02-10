e03181be5dcee413c782ba78cc000e8e1df6d6cf
ahmad-selim59 <ahmad22selim@gmail.com>
2026-02-10 13:55:40 +0000

    cors and nginx config file

--- src/main.py
+++ src/main.py
@@ -21,6 +21,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "https://language-chat-buddy-fe.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
